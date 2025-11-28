10d3328a7d1bab868a26cf503a4152297861af79
Naloee <nasra.ali3@gmail.com>
2025-11-28 18:26:06 +0100

    modif form

--- adaverse/seed/projets_ada.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO  projets_Ada (nom_prj) VALUES
-('Ada Quiz'), 
-('Dataviz'), 
-('Adaopte'), 
-('Adaence'), 
-('Adaction'), 
-('Ada Check Events');
--- adaverse/seed/publier_projet.sql
+++
@@ -1,3 +0,0 @@
-UPDATE prj_etudiant
-SET date_pub = CURRENT_DATE
-WHERE id = $1
