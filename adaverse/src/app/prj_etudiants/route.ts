d255588d31000debeea11bb272bbeb5465f948d2
meriemmhd <meriemmehdi{~@gmail.com>
2025-12-01 11:16:18 +0100

    recup

--- adaverse/src/app/prj_etudiants/route.ts
+++ adaverse/src/app/prj_etudiants/route.ts
@@ -1,4 +1,4 @@
-import { db,prjetudiant } from "../../db";
+import { db,prjetudiant } from "@/db";
 
 export async function GET() {
   const data = await db.select().from(prjetudiant);
@@ -7,3 +7,17 @@ export async function GET() {
   });
 }
 
+export async function POST (req:Request) {
+    const body = await req.json();
+  const result = await db.insert(prjetudiant).values({
+     titre: body.titre,
+     desc: body.desc,
+     adrs_web_perso:body.adrs_web_perso,
+     illustration: body.illustration,
+     lien_git: body,
+     lien_demo: body,
+     date_crea:body,
+     date_pub:body.date_pub,
+  });
+  return Response.json({success:true,result});
+};
