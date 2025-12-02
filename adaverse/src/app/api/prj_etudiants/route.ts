4bed2d561acc136bf22c6e9d73ff398c67d799ee
Naloee <nasra.ali3@gmail.com>
2025-12-02 23:39:33 +0100

    modif route

--- adaverse/src/app/api/prj_etudiants/route.ts
+++ adaverse/src/app/api/prj_etudiants/route.ts
@@ -1,7 +1,69 @@
+import { NextRequest, NextResponse } from "next/server";
 import { db,prjetudiant } from "@/db"
+
+
 export async function GET() {
+
+  try{
   const data = await db.select().from(prjetudiant);
-  return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" }
-  });
+   return NextResponse.json(data);
+  } catch (err) {
+    console.error("Erreur GET /api/prj_etudiants:", err);
+    return NextResponse.json({ error: "Erreur serveur" }, { status: 500 });
+  }
 }
+// export async function POST(req: NextRequest) {
+//   try {
+//     // Récupération des données JSON envoyées depuis le front
+//     const data = await req.json();
+
+//     // Validation minimale (optionnelle)
+//     const requiredFields = [
+//       "titre",
+//       "adrs_web_perso",
+//       "illustration",
+//       "lien_git",
+//       "lien_demo",
+//       "date_crea",
+//       "promotions_ada_id",
+//       "projets_ada_id",
+//     ];
+
+//     for (const field of requiredFields) {
+//       if (!data[field]) {
+//         return NextResponse.json(
+//           { error: `${field} est requis` },
+//           { status: 400 }
+//         );
+//       }
+//     }
+
+//      const promotionsId = Number(data.promotions_ada_id);
+//     const projetsId = Number(data.projets_ada_id);
+
+//     if (isNaN(promotionsId) || isNaN(projetsId)) {
+//       return NextResponse.json(
+//         { error: "IDs invalides" },
+//         { status: 400 }
+//       );
+//     }
+
+//     // Insertion dans la table prj_etudiant
+//     const inserted = await db.insert(prjetudiant).values({
+//       titre: data.titre,
+//       adrs_web_perso: data.adrs_web_perso,
+//       illustration: data.illustration,
+//       lien_git: data.lien_git,
+//       lien_demo: data.lien_demo,
+//       date_crea: new Date(data.date_crea),
+//       date_pub: data.date_pub ? new Date(data.date_pub) : null,
+//       promotions_ada_id: Number(data.promotions_ada_id),
+//       projets_ada_id: Number(data.projets_ada_id),
+//     }).returning();
+
+//     return NextResponse.json({ message: "Projet ajouté", projet: inserted[0] });
+//   } catch (err) {
+//     console.error("Erreur POST /api/prj_etudiants:", err);
+//     return NextResponse.json({ error: "Erreur serveur" }, { status: 500 });
+//   }
+// }
